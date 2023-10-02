2eb594edcf8b4e48a8515752f5e54f8d0a5cc6ab
Nwonye-Michael <49059581+Nwonye-Michael@users.noreply.github.com>
2023-10-02 22:29:18 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
+let sleeping = true
+
 document.addEventListener("DOMContentLoaded", function () {
-  const box = document.getElementById("box")
-
+  // Get a reference to the box element
+  const sleepingKoala = document.getElementById("sleeping-koala")
+  const wakeMeBtn = document.querySelector("#wake-me-button")
+  // Add a click event listener to the document
   document.addEventListener("click", (event) => {
-    const mouseX = event.clientX
-    const mouseY = event.clientY
+    // Get the mouse coordinates where the click occurred
 
-    // Calculate the position relative to the entire document
-    const rect = box.getBoundingClientRect()
-    const offsetX = mouseX - rect.width / 2
-    const offsetY = mouseY - rect.height / 2
+    if (event.target.id === "wake-me-button") {
+      console.log(event)
 
-    // Use GSAP to animate the box to the clicked position
-    gsap.to(box, { x: offsetX, y: offsetY, duration: 0.5 })
+      return null
+    } else {
+      console.log("body clicked")
+      console.log(event)
+      const mouseX = event.clientX
+      const mouseY = event.clientY
+
+      // Use GSAP to animate the box to the clicked position
+      gsap.to(sleepingKoala, { x: mouseX, y: mouseY, duration: 0.5 })
+    }
+  })
+
+  wakeMeBtn.addEventListener("click", () => {
+    if (sleeping) {
+      // wake it
+      sleepingKoala.src = "/assets/awake.png"
+      wakeMeBtn.textContent = "send to bed"
+      sleeping = false
+      console.log("changed to awake")
+    } else {
+      sleepingKoala.src = "/assets/sleeping.png"
+      wakeMeBtn.textContent = "wake me"
+      sleeping = true
+      console.log("changed to sleeping")
+    }
   })
 })
