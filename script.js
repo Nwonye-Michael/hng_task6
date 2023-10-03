1e9feeebb456aabd413fd5037b3ea1d106585980
Nwonye-Michael <49059581+Nwonye-Michael@users.noreply.github.com>
2023-10-03 17:50:51 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,42 +1,97 @@
-let sleeping = false
-
 document.addEventListener("DOMContentLoaded", function () {
-  // Get a reference to the box element
   const sleepingKoala = document.getElementById("sleeping-koala")
+
+  let sleeping = false
+  const awakeKoala = document.getElementById("awake-koala")
   const wakeMeBtn = document.querySelector("#wake-me-button")
-  // Add a click event listener to the document
-  document.addEventListener("click", (event) => {
-    // Get the mouse coordinates where the click occurred
 
-    if (event.target.id === "wake-me-button") {
-      console.log(event)
-
-      return null
+  wakeMeBtn.addEventListener("click", () => {
+    console.log("clicked")
+    if (!sleeping) {
+      // Wake it
+      awakeKoala.style.display = "none"
+      sleepingKoala.style.display = "flex"
+      wakeMeBtn.textContent = "wake me"
+      sleeping = true
+      console.log("changed to sleeping")
     } else {
-      console.log("body clicked")
-      console.log(event)
-      const mouseX = event.clientX
-      const mouseY = event.clientY
+      // Send it to bed
 
-      // Use GSAP to animate the box to the clicked position
-      gsap.to(sleepingKoala, { x: mouseX, y: mouseY, duration: 0.5 })
+      sleepingKoala.style.display = "none"
+      awakeKoala.style.display = "flex"
+      wakeMeBtn.textContent = "send to bed"
+      sleeping = false
+      console.log("changed to awake")
     }
   })
 
-  wakeMeBtn.addEventListener("click", () => {
-    if (sleeping) {
-      // wake it
-      sleepingKoala.src = "/assets/sleeping.png"
-      wakeMeBtn.textContent = "wake me"
-
-      sleeping = false
-      console.log("changed to awake")
+  wakeMeBtn.click()
+  wakeMeBtn.click()
+  wakeMeBtn.click()
+  document.addEventListener("click", (event) => {
+    if (event.target.id === "wake-me-button" || "makeitrainbtn") {
+      return null
     } else {
-      sleepingKoala.src = "/assets/awake.png"
+      console.log("body clicked")
 
-      wakeMeBtn.textContent = "send to bed"
-      sleeping = true
-      console.log("changed to sleeping")
+      const mouseX = event.clientX
+      const mouseY = event.clientY
+
+      gsap.to(sleepingKoala, { x: mouseX, y: mouseY, duration: 0.5 })
+      gsap.to(awakeKoala, { x: mouseX, y: mouseY, duration: 0.5 })
     }
+  })
+
+  const sun = document.querySelector(".sun")
+  const moon = document.querySelector(".moon")
+  const rain = document.querySelector(".rain")
+  const cloud = document.querySelector(".cloud")
+  const makeitRain = document.querySelector("#makeitrainbtn")
+
+  let isRaining = false
+
+  makeitRain.addEventListener("click", (event) => {
+    if (!isRaining) {
+      isRaining = true
+
+      gsap.to("body", {
+        backgroundColor: "#333",
+        duration: 2,
+      })
+
+      makeitRain.textContent = "Lights On"
+    } else {
+      isRaining = false
+
+      gsap.to("body", {
+        backgroundColor: "#c48f5c",
+        duration: 2,
+      })
+
+      makeitRain.textContent = "lights out"
+    }
+    const vwToPixels = (vw) => (vw * window.innerWidth) / 100
+    const vhToPixels = (vh) => (vh * window.innerHeight) / 100
+    const mouseX = event.clientX + vwToPixels(1)
+
+    const mouseY = event.clientY - vhToPixels(10)
+    gsap.to(sleepingKoala, {
+      x: mouseX,
+      y: mouseY,
+      duration: 2,
+      yoyo: true,
+    })
+    gsap.to(sleepingKoala, {
+      x: mouseX,
+      y: mouseY,
+      duration: 2,
+      yoyo: true,
+    })
+    gsap.to(awakeKoala, {
+      x: mouseX,
+      y: mouseY,
+      duration: 2,
+      yoyo: true,
+    })
   })
 })
